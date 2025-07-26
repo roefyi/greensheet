7e46910c2ba62c89cb14b952587a05dd19e2894f
roefyi <romansdenson@gmail.com>
2025-07-26 14:39:43 -0500

    Initial Commit

---
+++ Daybook/DaybookApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaybookApp.swift
+//  Daybook
+//
+//  Created by Rome on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct DaybookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
